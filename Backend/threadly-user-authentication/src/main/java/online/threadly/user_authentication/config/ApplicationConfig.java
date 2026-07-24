5858a9ad875f91fc0f0fd5ba394d586754f1b137
Utkarsh Jain <jainutkarsh096@gmail.com>
2026-07-24 05:51:51 +0530

    removed unused imports

--- Backend/threadly-user-authentication/src/main/java/online/threadly/user_authentication/config/ApplicationConfig.java
+++ Backend/threadly-user-authentication/src/main/java/online/threadly/user_authentication/config/ApplicationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
--- Backend/threadly-user-authentication/src/main/java/online/threadly/user_authentication/config/SecurityConfig.java
+++ Backend/threadly-user-authentication/src/main/java/online/threadly/user_authentication/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package online.threadly.user_authentication.config;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import online.threadly.user_authentication.filter.JwtAuthFilter;
 import online.threadly.user_authentication.service.OAuth2Successhandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
